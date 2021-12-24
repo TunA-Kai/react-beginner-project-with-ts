c9a6a8597328099c49c98ff948324fb48cfd61c8
TunA-Kai <emailtolearncode@gmail.com>
2021-12-24 15:07:45 +0700

    set up useReducer

--- src/Interface.ts
+++ src/Interface.ts
@@ -1,1 +1,11 @@
-export interface ContextT {}
+export enum ActionType {}
+
+export type StatusT = ''
+
+export interface StateT {}
+
+export type ActionT = {}
+
+export interface ContextT extends StateT {}
+
+export interface MovieT {}
---
+++ src/reducer.ts
@@ -0,0 +1,5 @@
+import { ActionT, StateT } from './Interface'
+
+export function reducer(state: StateT, action: ActionT): StateT {
+    return state
+}
