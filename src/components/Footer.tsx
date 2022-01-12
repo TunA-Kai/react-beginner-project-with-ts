2dbf1443ddf743f6a0b7b7667afb609b56cfd877
TunA-Kai <emailtolearncode@gmail.com>
2022-01-12 20:03:07 +0700

    update Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,14 @@ import styled from 'styled-components'
 interface FooterProps {}
 
 function Footer({}: FooterProps) {
-    return <>Footer Component</>
+    return (
+        <Wrapper>
+            <h5>
+                &copy; {new Date().getFullYear()} <span>Comfy Sloth</span>
+            </h5>
+            <h5>All rights reserved</h5>
+        </Wrapper>
+    )
 }
 
 export default Footer
